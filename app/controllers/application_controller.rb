9a8c9c3ac50abe7f498d5518fa48494a0cd02b9d
Dony Purnama <dony.purnama@gmail.com>
2017-06-30 09:44:44 +0700

    menambahkan field baru

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :email, :password])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :email, :password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :date_of_birth, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :date_of_birth, :email, :password])
   end
 end
---
+++ db/migrate/20170618182153_add_dateofbirth_to_users.rb
@@ -0,0 +1,6 @@
+class AddDateofbirthToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :date_of_birth, :datetime
+    add_column :users, :is_female, :boolean, default: false
+  end
+end
