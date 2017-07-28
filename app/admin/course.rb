084e0630725acf05fa9cd5fc0126f4812e99f1b4
Dony Purnama <dony.purnama@gmail.com>
2017-07-28 22:14:47 +0700

    manajemen course dan task

--- app/admin/course.rb
+++ app/admin/course.rb
@@ -1,16 +1,15 @@
 ActiveAdmin.register Course do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-permit_params :title, :description, :price, :status, :image
 
+  permit_params :title, :description, :price, :status, :image
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :description
+      f.input :price
+      f.input :status, as: :select, collection: ["Active", "Not Active"], include_blank: false
+      f.input :image
+      end
+      f.actions
+    end
 end
