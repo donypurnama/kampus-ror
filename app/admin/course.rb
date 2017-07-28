16c577384dc5b8fd1d208e25dbb3934ecc5652b8
Dony Purnama <dony.purnama@gmail.com>
2017-07-28 22:34:04 +0700

    course and task management

--- app/admin/course.rb
+++ app/admin/course.rb
@@ -2,13 +2,28 @@ ActiveAdmin.register Course do
 
   permit_params :title, :description, :price, :status, :image
 
+  show do
+    attributes_table do
+      row :title
+      row :description
+      row :price
+      row :status do
+        number_to_currency(course.price)
+      end
+      row :status
+      row :image do
+        course.image.present? ? image_tag(course.image.url, height: 100) : content_tag(:span, "no image")
+      end
+    end
+  end
+
   form do |f|
     f.inputs do
       f.input :title
       f.input :description
       f.input :price
       f.input :status, as: :select, collection: ["Active", "Not Active"], include_blank: false
-      f.input :image
+      f.input :image, hint: course.image.present
       end
       f.actions
     end
